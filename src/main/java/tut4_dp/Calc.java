42eb77fbb993332b123054508c55f7b73d7de467
“ssty” <1539925413@qq.com>
2022-09-24 17:37:12 +0800

    tut4_02_2

--- src/main/java/tut4_dp/Calc.java
+++ src/main/java/tut4_dp/Calc.java
@@ -7,7 +7,8 @@ public class Calc {
     public int add(int x, int y) {
         return x + y;
     }
+
     public int subtract(int x, int y) {
-        return x + y;
+        return x - y;
     }
 }
